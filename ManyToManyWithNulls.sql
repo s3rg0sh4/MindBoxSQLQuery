35e6fc5f17a1268fca7dbfa9d30bb84bc3182997
Сергей <s3rg0sh4@gmail.com>
2022-06-29 00:00:39 +0300

    russian to english

--- ManyToManyWithNulls.sql
+++ ManyToManyWithNulls.sql
@@ -1,4 +1,4 @@
-Select ProductName as Товар, CategoryName as Категория
+Select ProductName as Product, CategoryName as Category
 From MindboxTest.dbo.Product_Category as pc right join MindboxTest.dbo.Product as prod
 On pc.ProductID = prod.ProductID
 left join MindboxTest.dbo.Category as cat
